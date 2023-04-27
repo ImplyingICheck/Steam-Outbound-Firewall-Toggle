6d129b6958ed3be6665e04befdd41243573bc1ae
Hector Amador <amhector@stanford.edu>
2023-04-26 20:32:51 -0500

    Added CustomUserIO Module
    ## What?
    Rewrote Exit-OnKeyPress to be an independent module separate from the main script
    ## Why?
    Exiting the shell interface given user input is a prime function to be reused in other scripts. Creating this module allows portability of the Exit-OnKeyPress code and version control.
    ## How?
    Rewrote the Exit-OnKeyPress into its own module. The function was updated with in-file documentation.
    
    Additionally, some new functionality was implemented to increase portability. Namely, the function now allows deletion of the message and newline characters which were previously always included. New usability was added by allowing function calls to format the text after the default output.
    ## Testing?
    Verified to import and work without error when dot sourced into the main script.
    
    ## Anything Else?
    A module manifest should be created to ensure the module version stays locked.

---
+++ CustomUserIO.psm1
@@ -0,0 +1,70 @@
+# TODO: Create module manifest
+
+function Exit-OnKeyPress {
+    <#
+    .SYNOPSIS
+        Shows a message to the user, then exits upon a key press.
+    .DESCRIPTION
+        Shows a message to the end user through the Write-Information 
+		pipeline. The final form of the messages is 
+		"($Message)[`n][Press any key to exit...]($EndFormatting)"
+		
+		The script then ceases to execute.
+		
+		Checks if running Powershell ISE. If so, generates a default window
+		and alerts to user to intended end-user behaviour (using a differing 
+		instruction blurb).
+	.PARAMETER Message
+		An optional developer-designated message with no formating applied.
+	.PARAMETER EndFormatting
+		An optional end formatting and/or message to appear after the default 
+		instructions. Can also be used to replace the instruction blurb alongside
+		-NoInstructionBlurb
+	.PARAMETER NoInstructionBlurb
+		Allows removal of the default instruction blurb
+	.PARAMETER NoDefaultFormatting
+		Allows removal of the newline character prior to the instruction blurb.
+    .EXAMPLE
+        Exit-OnKeyPress "This is" "sandwiching the blurb" -NoDefaultFormatting
+	.EXAMPLE
+		Exit-OnKeyPress "Unexpected error occured" "<(@.@<)"
+	.EXAMPLE
+		Exit-OnKeyPress "This is" "<A custom error message>" -NoInstructionBlurb
+    #>
+    [CmdletBinding()]
+    param (
+		[Paramater(Mandatory=$false)]
+        [String]$Message = "",
+		
+		[Paramater(Mandatory=$false)]
+        [String]$EndFormatting = "",
+		
+		[Paramater(Mandatory=$false)]
+        [Switch]$NoInstructionBlurb,
+		
+		[Paramater(Mandatory=$false)]
+        [Switch]$NoDefaultFormatting
+    )
+    begin {
+    }
+    process {
+		$defaultNewline = if ($NoDefaultFormatting) {""} Else {"`n"}
+		$instructionBlurb = ""
+		if (-not $NoInstructionBlurb) {
+			$instructionBlurb = if ($PSIse) {"!!!THIS PROMPT WOULD EXIT ON ANY KEYPRESS FOR CONSOLE!!!"} else {"Press any key to exit..."}
+		}
+		$finalMessage = "{0}{1}{2}{3}" -f $Message, $defaultNewline, $instructionBlurb, $EndFormatting
+		if ($PSIse) {
+			Add-Type -AssemblyName System.Windows.Forms
+			$null = [System.Windows.Forms.MessageBox]::Show($finalMessage)
+		} else {
+			Write-Information $finalMessage
+			$null = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+		}
+		Exit
+    }
+    end {
+    }
+}
+
+Export-ModuleMember -Function Exit-OnKeyPress
