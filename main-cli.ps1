5f126c31e43b06d7f2c20bdbee8b76a3a38dccb4
Andrew <amhector@stanford.edu>
2023-06-23 19:06:19 -0700

    feat: Separate CLI integration from main script
    Implement main-clip.ps1. The CLI implementation of Toggle.

---
+++ main-cli.ps1
@@ -0,0 +1,22 @@
+#requires -version 5.1
+$ErrorActionPreference = "Stop"
+
+Write-Host $PSScriptRoot
+
+. ($PSScriptRoot + "\main.ps1")
+
+if (-not ( Test-IsAdministrator )) {
+    Start-AsAdministrator $PSCommandPath
+} else {
+    # Add check for outdated rule paths
+    if (-not ( Get-SWdfRule ) ) {
+        New-SWdfRule
+    }
+    if ( Switch-SWdfRule ) {
+        $verbage = "actively being smothered."
+    } Else {
+        $verbage = "currently allowed to breathe :)"
+    }
+    Write-Information ("Steam is {0}" -f $verbage)
+    Exit-OnKeyPress
+}
